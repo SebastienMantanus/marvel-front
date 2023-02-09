28d6a098f99c70c23d67e03b76e15e546fc67322
Sébastien Mantanus <sebastienmantanus@iMacdeSebastien.zyxel.localhost>
2023-02-09 22:05:08 +0100

    routes created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Characters from "./pages/Characters";
+import Comics from "./pages/Comics";
+import Starred from "./pages/Starred";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Character from "./pages/Character";
 
 function App() {
-  return <div>Coucou Marvel !</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Characters />} />
+        <Route path="/comics" element={<Comics />} />
+        <Route path="/favoris" element={<Starred />} />
+        <Route path="/character/:id" element={<Character />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
