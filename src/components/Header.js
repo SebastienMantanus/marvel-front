952cf14173c018de7d46681ed658d574a0a35971
Sébastien Mantanus <sebastienmantanus@MacBook-Pro-de-Sebastien.local>
2023-02-11 14:46:48 +0100

    header V1 ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
+import logo from "../assets/logo.png";
+
+import { Link } from "react-router-dom";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <img src={logo} alt="marvel visual" />
+      <div>
+        <Link to="/">Personnages</Link>
+        <Link to="/comics">Comics</Link>
+        <Link to="/favoris">Favoris</Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
