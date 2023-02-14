f367e1ed8e1391b225683064f71b3f685afe9913
Freemulder <113979673+SebastienMantanus@users.noreply.github.com>
2023-02-14 19:48:51 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Logo from "./assets/logo.png";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>
+
 );
