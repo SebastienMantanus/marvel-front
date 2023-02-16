db81ba8f19a97384c44173de0af130646e0324ac
Sébastien Mantanus <sebastienmantanus@iMac-de-Sebastien.local>
2023-02-16 21:16:48 +0100

    character comics page done

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -11,7 +11,7 @@ const Character = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://marvel--marvel--vqtmsgjlf7qx.code.run/character/${id}`
+          `https://marvel--marvel--vqtmsgjlf7qx.code.run/comics/${id}`
         );
         setData(response.data);
         setIsLoading(false);
@@ -25,20 +25,37 @@ const Character = () => {
   return (
     <div className="characters-main">
       {!isLoading ? (
-        <div className="charater-title">
-          <h1>{data.name}</h1>
-          {data.description && <p>{data.description}</p>}
+        <div className="character-main">
+          <div className="character-header">
+            <div>
+              <h1>{data.name}</h1>
+              {data.description && <p>{data.description}</p>}
+            </div>
+            <div>
+              <img
+                src={`${data.thumbnail.path}/portrait_xlarge.${data.thumbnail.extension}`}
+                alt="toto"
+              />
+            </div>
+          </div>
           <div className="character-details">
-            <img
-              src={`${data.thumbnail.path}/portrait_xlarge.${data.thumbnail.extension}`}
-              alt="toto"
-            />
             <div>
               <h2>Comics</h2>
-              {/* 
-              {data.comics.map( (element, index) => {
-                return <p>toto</p>;
-              })} */}
+              {data.comics.map((element, index) => {
+                return (
+                  <div key={index} className="character-comics">
+                    <img
+                      src={`${element.thumbnail.path}/portrait_xlarge.${element.thumbnail.extension}`}
+                      alt={element.title}
+                    ></img>
+                    <div>
+                      <h3>{element.title}</h3>
+
+                      <p>{element.description}</p>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
